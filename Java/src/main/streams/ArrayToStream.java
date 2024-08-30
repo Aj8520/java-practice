2157d804c3d6ab1fc5d25eff61a1c4faedc802bf
Ajay <ajayjadhav852000@gmail.com>
2024-08-30 10:20:25 +0530

    first commit for  java project creation

---
+++ Java/src/main/streams/ArrayToStream.java
@@ -0,0 +1,15 @@
+package main.streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayToStream {
+    // converting arrys to list 
+    public static void main(String[] args) {
+        List<Integer> number= Arrays.asList(1,2,3,4,5);
+
+        // primiteve 
+        
+    }
+    
+}
