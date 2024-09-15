a86c929be43c3a85af12b7090aa6ac093a03c87c
AJ8520 <ajayjadhav852000@gmail.com>
2024-09-15 21:48:15 +0530

    pattern program day1

---
+++ Java/src/main/pattern/StarPattern3.java
@@ -0,0 +1,35 @@
+package main.pattern;
+
+public class StarPattern3 {
+
+    // *
+    // **
+    // ***
+    // ****
+    // ***
+    // **
+    // *
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+        for (int i = 3; i >= 1; i--) {
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+    }
+
+}
---
+++ Java/src/main/pattern/StarPattern6.java
@@ -0,0 +1,53 @@
+package main.pattern;
+
+public class StarPattern6 {
+
+    /*
+     * *
+     **
+     ***
+     ****
+     ***
+     **
+     *
+     */
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+
+            for (int j = 4; j > i; j--) {
+
+                System.out.print(" ");
+
+            }
+
+            for (int j = 0; j < i; j++) {
+
+                System.out.print("*");
+
+            }
+
+            System.out.println();
+
+        }
+
+        for (int i = 3; i >= 1; i--) {
+
+            for (int j = 4; j > i; j--) {
+
+                System.out.print(" ");
+
+            }
+
+            for (int j = 0; j < i; j++) {
+
+                System.out.print("*");
+
+            }
+
+            System.out.println();
+
+        }
+    }
+
+}
